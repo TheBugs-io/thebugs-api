e32a868f2107326fed6ae4813d4e0a4b0d73cd24
Ingryd <ingrydcduarte@gmail.com>
2025-06-15 23:14:45 -0300

    fix: using reservasRoutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
-import authRoutes from './routes/authRoutes.js';
+//import authRoutes from './routes/authRoutes.js';
+import reservasRoute from './routes/reservaRoutes.js';
 
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', authRoutes);
+//app.use('/api/auth', authRoutes);
+app.use('/reservas', reservasRoute);
 
 export default app;
