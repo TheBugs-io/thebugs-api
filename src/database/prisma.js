7484f6d721ec942e5559646ccd1505b8e2d8f78b
Ingryd <ingrydcduarte@gmail.com>
2025-06-15 23:09:49 -0300

    hotfix: solving es module + output prisma

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../../generated/prisma/index.js';
 
-let prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default prisma;
