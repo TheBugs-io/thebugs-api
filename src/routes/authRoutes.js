f470c08b70061e9e74fdcfb8d282f8f7f182ded5
Ingryd <ingrydcduarte@gmail.com>
2025-06-28 18:47:46 -0300

    feat: routes in auth routes (register service)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import * as authService from "../auth/authService.js";
 import * as registerService from "../auth/registerService.js";
+import { authMiddleware } from '../middleware/authMiddleware.js';
+import { autorizacaoSecretario } from '../middleware/authorizationAdm.js';
 
 const router = express.Router();
 
 router.post('/login', authService.login);
-router.post('/register', registerService.solicitarRegistro);
+router.post('/register/solicitar', registerService.solicitarRegistro);
+router.get('/register/confirmar/:token', registerService.confirmarEmail);
+//MARK: - Secretarios
+router.post('/register/confirm-user', autorizacaoSecretario, registerService.confirmarCriacaoUsuario);
+router.get('/register/token/:token', registerService.buscarPorToken);
+router.get('/register/id/:id', registerService.buscarPorId);
+router.get('/register/email/:email', registerService.buscarPorEmail);
+router.get('/register/pendentes', registerService.listarPendentes);
+router.patch('/register/:id/status', registerService.atualizarStatus);
+router.delete('/register/:id', registerService.deletarSolicitacao);
 
 export default router;
