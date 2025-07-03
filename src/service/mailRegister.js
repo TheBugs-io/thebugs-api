3383e9fd93c8d23857c2b929c1681f434df2bb8a
Ingryd <ingrydcduarte@gmail.com>
2025-07-02 23:23:16 -0300

    feat(wip): notificacao status registro (mail -> desaprovado)

--- src/service/mailRegister.js
+++ src/service/mailRegister.js
@@ -1,14 +1,15 @@
-import { createTransport } from 'nodemailer';
-import prisma from '../database/prisma.js';
-import { gerarHtmlSecretariaContent } from '../template/mailRequestRegister.js'
-import { mailConfirmToken } from '../template/mailEmailConfirm.js'
+import { createTransport } from "nodemailer";
+import prisma from "../database/prisma.js";
+import { gerarHtmlSecretariaContent } from "../template/mailRequestRegister.js";
+import { mailConfirmToken } from "../template/mailEmailConfirm.js";
+import { disapprovedRegisterRequest } from "../template/mailDisapprovedRequest.js"
 
 const transporter = createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 export const enviarEmailConfirmacao = async (email, token) => {
@@ -18,27 +19,37 @@ export const enviarEmailConfirmacao = async (email, token) => {
     //TODO: Mudar pro email próprio da aplicação (provavel criar)
     from: '"Cadê a Sala?" <nao-responda@ufc.br>',
     to: email,
-    subject: 'Confirmação de Cadastro - Cadê a Sala?',
-    html: mailConfirmToken(link)
+    subject: "Cadê a Sala? | Confirmação de Cadastro",
+    html: mailConfirmToken(link),
   });
 };
 
 export const notificarSecretaria = async (solicitacao) => {
   const admins = await prisma.usuario.findMany({
-    where: { tipo: 'SECRETARIO' },
-    select: { email: true }
+    where: { tipo: "SECRETARIO" },
+    select: { email: true },
   });
 
   if (admins.length === 0) return;
 
-  const emails = admins.map(a => a.email).join(',');
-  
+  const emails = admins.map((a) => a.email).join(",");
+
   await transporter.sendMail({
     //TODO: Mudar pro email próprio da aplicação (provavel criar)
     from: '"Cadê a Sala?" <nao-responda@ufc.br>',
     to: emails,
-    subject: 'Nova solicitação de cadastro',
+    subject: "Cadê a Sala? | Nova solicitação de cadastro",
     text: "Olá,\n\nhouve novo pedido de criação de conta na plataforma. Por favor, analise o pedido de registro de usuário.\n\nAtenciosamente,\nEquipe do Cadê a sala?",
-    html: gerarHtmlSecretariaContent(solicitacao)
+    html: gerarHtmlSecretariaContent(solicitacao),
+  });
+};
+
+export const notificarStatusRegistro = async (solicitacao) => {
+  //TODO: Mudar pro email próprio da aplicação (provavel criar)
+  await transporter.sendMail ( {
+    from: '"Cadê a Sala?" <nao-responda@ufc.br>',
+    to: solicitacao.email,
+    subject: "Cadê a Sala? | Solicitação de registro foi rejeitada",
+    text: disapprovedRegisterRequest(solicitacao)
   });
 };
