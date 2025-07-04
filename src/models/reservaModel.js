ece4db1bca5c6614d56899aaef8ffe64ba79282e
Ingryd <90623907+ingrydf12@users.noreply.github.com>
2025-07-04 14:56:56 -0300

    wip: validation fields in solicitarReserva function model

--- src/models/reservaModel.js
+++ src/models/reservaModel.js
@@ -3,3 +3,23 @@ import prisma from '../database/prisma.js';
 export const listarReservas = async () => {
     return await prisma.reserva.findMany();
 }
+
+export const solicitarReserva = async () => {
+    try {
+        const camposObrigatorios = { sala, responsavel, dataInicio, dataFim, horarioInicio, horarioFim }
+        const camposFaltando = Object.entries(camposObrigatorios)
+            .filter(([_, valor]) => !valor)
+            .map(([campo]) => campo);
+
+        if (camposFaltando.length > 0) {
+            return res.status(400).json({
+                error: "Campos obrigatórios faltando!",
+                campos: camposFaltando,
+            });
+        }
+
+        //TODO: - Envio de um modelo intermediário de solicitação, assim como registro.
+    } catch (error) {
+        return res.status(500).json({error: "Erro ao processar sua solicitação de reserva. Por favor, tente novamente.", });
+    }
+}
