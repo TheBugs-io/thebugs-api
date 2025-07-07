51877d002662a82a6235311b9c286d1282ca9f4a
Ingryd <ingrydcduarte@gmail.com>
2025-07-07 20:17:53 -0300

    feat: solicitarReserva in controller

--- src/controllers/reservaController.js
+++ src/controllers/reservaController.js
@@ -8,3 +8,32 @@ export const listarReservas = async (req, res) => {
         res.status(500).json({erro: error.message || "Erro ao listar!"});
     }
 }
+
+export const solicitarReserva = async (req, res) => {
+  try {
+    const {
+      localId,
+      tipo,
+      dataInicio,
+      dataFim,
+      usuarioId,
+    } = req.body;
+
+    const solicitacao = await reservaModel.solicitarReserva({
+      localId,
+      tipo,
+      dataInicio,
+      dataFim,
+      usuarioId,
+    });
+
+    res.status(200).json({
+      message: "Reserva solicitada. Aguarde a aprovação da secretaria.",
+      solicitacao,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "Erro ao solicitar sua reserva.",
+    });
+  }
+};
