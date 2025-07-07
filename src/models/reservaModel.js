5dde8d9bb47975442259b3ef9f9b4be3924688f4
Ingryd <ingrydcduarte@gmail.com>
2025-07-07 20:17:38 -0300

    feat: promise in solicitarReserva (local and usuario exists?)

--- src/models/reservaModel.js
+++ src/models/reservaModel.js
@@ -4,44 +4,41 @@ export const listarReservas = async () => {
   return await prisma.reserva.findMany();
 };
 
-export const solicitarReserva = async (req, res) => {
-  try {
-    const {
+export const solicitarReserva = async (dadosReserva) => {
+  const {
+    localId,
+    tipo,
+    dataInicio,
+    dataFim,
+    usuarioId,
+  } = dadosReserva;
+
+  const camposObrigatorios = { localId, tipo, dataInicio, dataFim, usuarioId };
+
+  const camposFaltando = Object.entries(camposObrigatorios)
+    .filter(([_, valor]) => valor === undefined || valor === null)
+    .map(([campo]) => campo);
+
+  if (camposFaltando.length > 0) {
+    throw new Error(`Campos obrigatórios faltando: ${camposFaltando.join(", ")}`);
+  }
+
+  //Os dois tem que existir pra que a solicitação seja feita, aqui com a Promise eu testo os dois casos, e retorno os erros pelos ifs em baixo
+  const [local, usuario] = await Promise.all([
+    prisma.local.findUnique({ where: { id: localId } }),
+    prisma.usuario.findUnique({ where: { id: usuarioId } })
+  ]);
+
+  if (!local) throw new Error("Local informado não existe.");
+  if (!usuario) throw new Error("Usuário informado não existe.");
+
+  return await prisma.solicitacaoReserva.create({
+    data: {
       localId,
       tipo,
-      dataInicio,
-      dataFim,
+      dataInicio: new Date(dataInicio),
+      dataFim: new Date(dataFim),
       usuarioId,
-    } = req.body;
-
-    const camposObrigatorios = { localId, tipo, dataInicio, dataFim, usuarioId };
-
-    const camposFaltando = Object.entries(camposObrigatorios)
-      .filter(([_, valor]) => valor === undefined || valor === null)
-      .map(([campo]) => campo);
-
-    if (camposFaltando.length > 0) {
-      return res.status(400).json({
-        error: "Campos obrigatórios faltando!",
-        campos: camposFaltando,
-      });
-    }
-
-    const novaSolicitacao = await prisma.solicitacaoReserva.create({
-      data: {
-        localId,
-        tipo,
-        dataInicio: new Date(dataInicio),
-        dataFim: new Date(dataFim),
-        usuarioId,
-      },
-    });
-
-    return res.status(201).json(novaSolicitacao);
-  } catch (error) {
-    console.error("Erro ao solicitar reserva:", error);
-    return res.status(500).json({
-      error: "Erro interno ao criar solicitação de reserva.",
-    });
-  }
+    },
+  });
 };
