01b860f68561a7ccc6b6e7ea219419b067d337b4
Ingryd <ingrydcduarte@gmail.com>
2025-07-07 23:02:22 -0300

    feat: delete and get by user routes

--- src/routes/reservaRoutes.js
+++ src/routes/reservaRoutes.js
@@ -10,5 +10,7 @@ router.get('/', reservaController.listarReservas)
  // Protege as rotas abaixo com autenticação
 router.use(authMiddleware)
 router.post('/solicitar-reserva', reservaController.solicitarReserva)
+router.delete('/:id', reservaController.cancelarReserva);
+router.get('/minhas-reservas', reservaController.listarReservasUsuario);
 
 export default router;
