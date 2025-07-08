33e295e0e78459f77a0eac4267e66dad6ee194da
Ingryd <ingrydcduarte@gmail.com>
2025-07-07 21:53:04 -0300

    auth: passing authorization in reservaRoutes

--- src/routes/reservaRoutes.js
+++ src/routes/reservaRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import * as reservaController from '../controllers/reservaController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+import { autorizacaoSecretario } from '../middleware/authorizationAdm.js';
 
 const router = express.Router();
-
+// Protege as rotas abaixo com autorização de secretário
+router.use(autorizacaoSecretario)
 router.get('/', reservaController.listarReservas)
+ // Protege as rotas abaixo com autenticação
+router.use(authMiddleware)
+router.post('/solicitar-reserva', reservaController.solicitarReserva)
 
 export default router;
