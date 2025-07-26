b2147cc888d471075bfc859a8b4c753265bca3c0
Ingryd <ingrydcduarte@gmail.com>
2025-07-26 14:42:09 -0300

    fix: variable

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -11,7 +11,7 @@ export const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.usuario = decoded;
+    req.user = decoded;
     next();
   } catch (err) {
     console.log("Erro no authMiddleware:", err.message);
