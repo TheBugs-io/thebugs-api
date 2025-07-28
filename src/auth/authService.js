e831d9cb3b48376deda7ac10ddc866905eaf3694
Ingryd <ingrydcduarte@gmail.com>
2025-07-27 21:07:06 -0300

    feat: forgot / reset password (servico)

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import * as userController from '../controllers/usuariosController.js'
+import * as userController from "../controllers/usuariosController.js";
+import { verificarRedefinicaoEmail } from "../service/mailRegister.js";
 
 export const login = async (req, res) => {
   try {
@@ -24,20 +25,77 @@ export const login = async (req, res) => {
       { expiresIn: "1h" }
     );
 
-  return res.status(200).json({
-    message: "Login realizado com sucesso",
-    usuario: {
-      token,
-      id: usuario.id,
-      nome: usuario.nomeCompleto,
-      email: usuario.email,
-      tipo: usuario.tipo,
-    },
-  });
+    return res.status(200).json({
+      message: "Login realizado com sucesso",
+      usuario: {
+        token,
+        id: usuario.id,
+        nome: usuario.nomeCompleto,
+        email: usuario.email,
+        tipo: usuario.tipo,
+      },
+    });
   } catch (error) {
     console.error("Erro no login:", error);
     res.status(500).json({ error: "Erro ao fazer login!" });
   }
 };
 
-//TODO: Request change password (PUT)
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: "Email é obrigatório." });
+  }
+  try {
+    const usuario = await userController.buscarUsuarioPorEmail(email);
+
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+    const token = jwt.sign({ userId: usuario.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    const link = `${process.env.FRONTEND_URL}/reset-password?token=${token}`;
+    await verificarRedefinicaoEmail(usuario.email, link);
+    return res.status(200).json({ message: "E-mail de recuperação enviado." });
+  } catch (error) {
+    console.error("Erro ao enviar e-mail de recuperação:", error);
+    return res
+      .status(500)
+      .json({ error: "Erro ao enviar e-mail de recuperação." });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { token, novaSenha } = req.body;
+
+  if (!token || !novaSenha) {
+    return res
+      .status(400)
+      .json({ error: "Token e nova senha são obrigatórios." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const usuarioId = decoded.userId;
+
+    const usuario = await userController.buscarUsuarioPorId(usuarioId);
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuário não encontrado." });
+    }
+
+    const senhaCriptografada = await bcrypt.hash(novaSenha, 10);
+
+    await userController.atualizarSenha(usuarioId, senhaCriptografada);
+
+    return res.status(200).json({ message: "Senha redefinida com sucesso." });
+  } catch (error) {
+    console.error("Erro ao redefinir senha:", error);
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expirado." });
+    }
+
+    return res.status(400).json({ error: "Token inválido." });
+  }
+};
